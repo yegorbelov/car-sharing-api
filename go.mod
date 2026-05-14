49027460302d222d8b451fb5137253e5d7f8a504
Egor Belov <ebelov54@gmail.com>
2026-05-14 17:30:15 +0300

    chore: init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module car-sharing-api
+
+go 1.25.0
+
+require (
+	github.com/labstack/echo/v5 v5.1.1 // indirect
+	golang.org/x/time v0.14.0 // indirect
+)
---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.RequestLogger())
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	if err := e.Start(":1323"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
